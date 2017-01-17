11791656966db1388c1ef1c05ff594c8b3723cf3
SNVSNANNAPA <SNVSNANNAPA@gmail.com>
2017-01-17 09:11:05 +0000

    Add files via upload

--- CopyReports.sh
+++ CopyReports.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 
+mainDir="c:\\testnow"
+cd $mainDir/code
 ruby genreport.rb
-basePath="C:\\testnow"
-source "$basePath/jobparams.sh"
-mkdir "${basePath}/code/${OUTPUT_DIR}"
 
-while [ ! -f "$basePath/dummy.txt" ]
-do
-  echo "Sleeping for 5 seconds....." >> "c:\\TestNow/sleep.txt"
-  sleep 5
-done
-
-#Init Run Count
-runCount=`cat $basePath/RunCount.txt`
-export RUN_COUNT=$runCount
-
-cd $basePath
-sh copyReportsAndNotifyTestNow.sh
-
-nohup sudo sh #{basePath}/cleanup.sh &
-echo "Automation test execution done............................................................."
+cd $mainDir
+source CopyReports.sh
